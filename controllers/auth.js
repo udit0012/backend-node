0a6f575d962be00c8f24cce64a93e482e1bec268
Ayush Rawal <royalrawal.2001@gmail.com>
2023-03-01 22:48:54 +0530

    basic auth wip

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,28 +14,27 @@ export const register = async (req, res) => {
         res.status(400).json({ "error": "Password can not be empty" })
     }
 
-    let user = User.findAll({
+    let user = await User.findAll({
         where: {
             email: email
         }
     })
 
-    if (user) {
+    if (user.length > 0) {
         res.status(409).json({ "error": "Email already registered" })
+        return
     }
     try {
         const salt = genSaltSync(10);
         password = hashSync(password, salt);
 
         user = await User.create({ email: email, password: password })
-        const jsontoken = jsonwebtoken.sign({ user: user }, process.env.SECRET_KEY, { expiresIn: '30m' });
+        const jsontoken = jsonwebtoken.sign({ user: user }, process.env.SECRET_KEY || "supersecret", { expiresIn: '30m' });
         res.cookie('token', jsontoken, { httpOnly: true, secure: true, SameSite: 'strict', expires: new Date(Number(new Date()) + 30 * 60 * 1000) }); //we add secure: true, when using https.
-
-
+        res.status(200)
         res.json({ token: jsontoken, email: email });
     } catch (e) {
         console.log(e)
     }
-
 }
 
